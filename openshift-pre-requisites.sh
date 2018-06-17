2e24006948c890f0c7caf49c4d4d8e429047cb00
Ashok Kalakoti <kashu9999@gmail.com>
2018-06-17 14:42:58 +0530

    adding shell script for openshift recommendations

---
+++ openshift-pre-requisites.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Updating the Packages
+
+yum update -y
+
+# Installing the Epel Repository
+
+yum install epel-release -y
+
+# Installing the Python-pip Repository
+yum install python-pip git python-lxml -y 
+
+# Clone the Openshift Repository into root Home directory
+
+git clone https://github.com/openshift/openshift-ansible.git /root/openshift-ansible
+
+# Go to the Openshit Cloned repo then install the requirements
+
+cd /root/openshift-ansible
+
+# Ansible Installation
+
+pip install -r requirements.txt
