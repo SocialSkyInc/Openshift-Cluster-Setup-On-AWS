7e7f9bae3ed1e219d0ed985a6127c28154b5fa97
Ashok Kalakoti <kashu9999@gmail.com>
2018-06-17 15:29:06 +0530

    adding command to install Developement tools in Linux Server

--- openshift-pre-requisites.sh
+++ openshift-pre-requisites.sh
@@ -9,7 +9,7 @@ yum update -y
 yum install epel-release -y
 
 # Installing the Python-pip Repository
-yum install python-pip git python-lxml -y 
+yum install python-pip git python-lxml python-devel libevent-devel -y 
 
 
 # Install Development Tools
@@ -26,3 +26,21 @@ cd /root/openshift-ansible
 # Ansible Installation
 
 pip install -r requirements.txt
+
+# Clone my repository to get the Pre-Configured Ansible host file and the Official Open repo to install the Packages
+git clone https://github.com/kashu9999/Openshift-Cluster-Setup-On-AWS.git /root/my-openshift-setup
+
+# Copy the repo file into yum.repos.d/
+
+cp -rvf /root/my-openshift-setup/open.repo /etc/yum.repos.d/open.repo
+
+#Take the backup of Original Ansible Host file
+
+cp -rvf /etc/ansible/hosts /etc/ansible/hosts.bkp
+
+# Copy the my Ansible Host file 
+
+cp -rvf /root/my-openshift-setup/hosts /etc/ansible/hosts
+
+
+
